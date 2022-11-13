9baa8fee4b012ca3d2a15b64fdf14416dfa9c63f
Emanuel Silva Araujo <Emanuelsa-rv@hotmail.com>
2022-11-12 22:41:44 -0300

    Feat: definitions of styles types

---
+++ lib/constants/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+const sectionTitleStyle = TextStyle(fontSize: 30, 
+        fontWeight: FontWeight.bold,
+        );
+
+const titleStyle = TextStyle(fontSize: 20, 
+        fontWeight: FontWeight.bold,
+        );
