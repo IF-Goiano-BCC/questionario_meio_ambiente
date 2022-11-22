0a6c0d68fc868a291dc5c715be69ab3e6d3c1ff7
Emanuel Silva Araujo <Emanuelsa-rv@hotmail.com>
2022-11-22 11:43:44 -0300

    Feat: title style

--- lib/constants/styles.dart
+++ lib/constants/styles.dart
@@ -5,6 +5,11 @@ const sectionTitleStyle = TextStyle(fontSize: 30,
         fontWeight: FontWeight.bold,
         );
 
+
+const Titleapp = TextStyle(fontSize: 50, 
+        fontWeight: FontWeight.bold,
+        );
+
 const titleStyle = TextStyle(fontSize: 20, 
         fontWeight: FontWeight.bold,
         );
